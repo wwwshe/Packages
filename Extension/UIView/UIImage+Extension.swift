d18402c1ae370e88f7131a4842e4e76f443aafbf
wwwshe1 <wwwshe1@nate.com>
2024-03-17 17:00:32 +0900

    resizeImage 추가

--- Extension/UIView/UIImage+Extension.swift
+++ Extension/UIView/UIImage+Extension.swift
@@ -50,6 +50,17 @@ public extension UIImage {
         
         return UIImage(cgImage: imageReference)
     }
+    
+    func resizeImage(newWidth: CGFloat) -> UIImage {
+        let scale = newWidth / self.size.width
+        let newHeight = self.size.height * scale
+        UIGraphicsBeginImageContext(CGSize(width: newWidth, height: newHeight))
+        self.draw(in: CGRect(x: 0, y: 0, width: newWidth, height: newHeight))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return newImage!
+    }
 }
 
 public extension UIImage {
